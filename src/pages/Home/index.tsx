35efe97dcbc63a588ee5613dfca5e1aa73949ec6
Arthur Rosch <rosch.arthur@gmail.com>
2023-02-01 17:57:04 -0300

    Estrutura visual - Componente: Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,9 +6,15 @@ export function Home() {
     <ContainerHome>
       <ContainerFilter>
         <select name="" id="">
+          <option value="" disabled selected>
+            Categoria
+          </option>
           <option value=""></option>
         </select>
         <select name="" id="">
+          <option value="" disabled selected>
+            Ordenar
+          </option>
           <option value=""></option>
         </select>
       </ContainerFilter>
